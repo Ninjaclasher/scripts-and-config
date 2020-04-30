3e90972be3d9c3820066b59ea4e5bca97f2ad1d9
Evan <evanzhang1028@hotmail.com>
2020-04-29 20:24:27 -0400

    Clean up competitive programming tools scripts

--- competitive-programming-tools/templates/check.py
+++ competitive-programming-tools/templates/check.py
@@ -2,6 +2,7 @@ from dmoj.result import CheckerResult
 from dmoj.error import InternalError
 from dmoj.utils.unicode import utf8text
 
+
 def check(proc_output, judge_output, judge_input, point_value, submission_source, **kwargs):
     try:
         proc_output = utf8text(proc_output)
@@ -9,7 +10,7 @@ def check(proc_output, judge_output, judge_input, point_value, submission_source
         judge_output = utf8text(judge_output)
         submission_source = utf8text(submission_source).replace('\r', '')
 
-    except (ValueError, IndexError, TypeError, OverflowError) as e:
-        return CheckerResult(False, 0)
+    except (ValueError, IndexError, TypeError, OverflowError):
+        return False
     except Exception as e:
         raise InternalError('Error in checker - ' + str(e))
