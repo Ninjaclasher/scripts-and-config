4856a0564568c4735ec2ad9180f5d2a7b1a86ada
Evan <evanzhang1028@hotmail.com>
2020-12-06 18:00:50 -0500

    polybar: cleanup config file and change underline colours

--- dotfiles/polybar/.config/polybar/launch.sh
+++ dotfiles/polybar/.config/polybar/launch.sh
@@ -2,8 +2,9 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(polybar -m | cut -d":" -f1); do
